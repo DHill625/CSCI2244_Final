9ec1886f23db5b2a54ec93ba3813d09f3927dcb1
mklein241 <78335386+mklein241@users.noreply.github.com>
2022-05-01 17:06:29 -0400

    Update find_best_guess.py

--- find_best_guess.py
+++ find_best_guess.py
@@ -1,9 +1,30 @@
+import math
 
+#VARIABLE DECLARATIONS
 max_info = 0
 cs_list = []            #length = 343 ex: SXXGS
-guess_list = []         #len 13,000
-answer_list = []        #originally len 2315
+guess_list = ["porch", "heist", "zesty", "aabbe", "cleep"]         #len 13,000
+answer_list = ["porch", "heist", "zesty"]        #originally len 2315
 info_dict = {}
+count = 0
+
+#FIND NEXT GUESS
+for g in guess_list:
+    info_sum = 0
+    for c in cs_list:
+        count = 0
+        for a in answer_list:
+            #if answer g fits into color scheme c given correct answer a
+                count += 1
+        prob_c = count/len(answer_list)
+        info = prob_c * log((1/prob_c), 2)
+        info_sum += info
+    expected_info = info_sum/343                #is div by 343 right?
+    info_dict[g] = expected_info
+    if expected_info > max_info:
+        max_info = expected_info
+        
+guess = info_dict.index(max_info)
 
 
 
